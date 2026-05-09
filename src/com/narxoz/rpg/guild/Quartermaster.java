f122d77b389a19cb7d8118e98fa6687eb2c32525
Nurs8t <nurseiit.atystanbek@narxoz.kz>
2026-05-09 21:39:33 +0500

    update Quartermaster class

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -10,12 +10,12 @@ public class Quartermaster extends GuildMember {
     }
 
     public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.printf("  [Quarter %-12s] received [%s] from %-14s: %s%n",
+                getName(), topic, from.getName(), payload);
     }
 }
