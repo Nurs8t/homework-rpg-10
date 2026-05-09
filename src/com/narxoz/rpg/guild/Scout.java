fce862eaede226617de94a6cf723e4c64dfb78c3
Nurs8t <nurseiit.atystanbek@narxoz.kz>
2026-05-09 21:38:03 +0500

    update Scout class

--- src/com/narxoz/rpg/guild/Scout.java
+++ src/com/narxoz/rpg/guild/Scout.java
@@ -10,12 +10,12 @@ public class Scout extends GuildMember {
     }
 
     public void reportRoute(String topic, String payload) {
-        // TODO: send a scouting message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.printf("  [Scout   %-12s] received [%s] from %-14s: %s%n",
+                getName(), topic, from.getName(), payload);
     }
 }
