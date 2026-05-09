bd195e688769138da87f48fbf6f2215592383433
Nurs8t <nurseiit.atystanbek@narxoz.kz>
2026-05-09 21:38:36 +0500

    update Healer class

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -10,12 +10,12 @@ public class Healer extends GuildMember {
     }
 
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.printf("  [Healer  %-12s] received [%s] from %-14s: %s%n",
+                getName(), topic, from.getName(), payload);
     }
 }
